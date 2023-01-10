11486c3744db713b635e964c9ae89b3237fd3ce6
vKaparyk <vlad.kaparykha@gmail.com>
2023-01-10 01:02:19 -0500

    stop inf loop

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ def main():
     board = Board(DIFFICULTY[0], DIFFICULTY[2])
 
     initialClick()
+    new_cells_counter = 0
     try:
         while True:
             new_cells = update_board(board)
+            if not new_cells:
+                new_cells_counter += 1
+            else:
+                new_cells_counter = 0
+                
+            if new_cells_counter > 2:
+                break
             mainAlgorithm(board, new_cells)
     except KeyboardInterrupt:
         exit()
